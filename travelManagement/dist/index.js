f5896eacd72f1abab92254f428be542bbfd05126
Juan Pablo Dominguez <juan.avenia@correounivalle.edu.co>
2023-05-02 00:25:55 -0500

    Deleting autogenerated folders

--- travelManagement/dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var express = require('express');
-var indexRouter = require('./api/routes/travelManagementRoutes');
-const app = express();
-const port = process.env.PORT || 3000;
-app.listen(port);
-console.log('todo list RESTful API server started on: ' + port);
-app.use('/', indexRouter);
