0a7c28ee54a8583bc2fcbd0a6307bec8fe4ea8db
Juan Pablo Dominguez <juan.avenia@correounivalle.edu.co>
2023-06-12 10:57:10 -0500

    Adding a database and cors

--- travelManagement/api/dataBase/databaseConnection.ts
+++ travelManagement/api/dataBase/databaseConnection.ts
@@ -1,19 +1,19 @@
 import { MongoClient } from 'mongodb';
 
 export class DatabaseConnection {
+	async connect() {
+		const uri =
+			'mongodb+srv://admin:travelManager123@travelmanagement.pfxm32i.mongodb.net/';
+		const dbName = 'travelManagementDB';
 
-    async connect() {
-        const uri = 'mongodb://127.0.0.1:27017';
-        const dbName = 'travelManagementDB';
+		const client = new MongoClient(uri);
 
-        const client = new MongoClient(uri);
-
-        try {
-            await client.connect();
-            console.log('Conectado a la base de datos');
-            return client;
-        } catch (error) {
-            console.error('Error al conectar a la base de datos', error);
-        }
-    }
+		try {
+			await client.connect();
+			console.log('Conectado a la base de datos');
+			return client;
+		} catch (error) {
+			console.error('Error al conectar a la base de datos', error);
+		}
+	}
 }
--- travelManagement/index.ts
+++ travelManagement/index.ts
@@ -1,7 +1,10 @@
 var express = require('express');
 var indexRouter = require('./api/routes/travelManagementRoutes');
+var cors = require('cors');
+
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3666;
+app.use(cors());
 app.use(express.json());
 
 app.listen(port);
