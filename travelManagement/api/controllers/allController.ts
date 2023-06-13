5f54b50bc97fdef329cb7e886dff9737e0091f28
Juan Pablo Dominguez <juan.avenia@correounivalle.edu.co>
2023-06-13 12:19:04 -0500

    Update some dafault paramenters and controller

--- travelManagement/api/controllers/allController.ts
+++ travelManagement/api/controllers/allController.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from 'express';;
+import { Request, Response } from 'express';
 import { Input, defaults } from '../models/Input/input.model';
 import { AllService } from '../services/allService';
 import { Place } from '../models/Input/place.model';
@@ -6,17 +6,17 @@ import { Place } from '../models/Input/place.model';
 const allService = new AllService();
 
 const orig: Place = {
-	IATA: "CLO",
-	name: "Cali"
-}
+	IATA: 'CLO',
+	name: 'Cali',
+};
 
 const dest: Place = {
-	IATA: "TCO",
-	name: "Tumaco"
-}
+	IATA: 'TCO',
+	name: 'Tumaco',
+};
 const inputData: Input = {
-	origin : orig,
-	destination : dest,
+	origin: orig,
+	destination: dest,
 	startDate: new Date(2023, 6, 1),
 	endDate: new Date(2023, 6, 2),
 	startTime: [new Date(2023, 6, 1, 0, 0)],
@@ -36,20 +36,23 @@ const inputData: Input = {
 	bathrooms: 1,
 	isSuperHost: false,
 	allowPolicies: [0, 1, 2, 3, 4],
-	features: []//TCO[4,5,8,12,33,47],
+	features: [], //TCO[4,5,8,12,33,47],
 };
 
 export async function sendAllInformation(req: Request, res: Response) {
-	var requirements: Input = inputData
+	var requirements: Input = {
+		...req.body,
+		...defaults,
+	};
 
-	/*var tempDate = new Date(req.body.endDate)
-	tempDate.setHours(23,59,0)
-	console.log(req)
+	var tempDate = new Date(req.body.endDate);
+	tempDate.setHours(23, 59, 0);
+	console.log(requirements);
 
 	requirements.startDate = new Date(req.body.startDate);
 	requirements.endDate = new Date(req.body.endDate);
 	requirements.startTime = [new Date(req.body.startDate)];
-	requirements.endTime = [tempDate];*/
+	requirements.endTime = [tempDate];
 	const allSolutions = await allService.getAllSolutions(requirements);
 
 	res.send(allSolutions);
--- travelManagement/api/services/externalFlightsAPIService.ts
+++ travelManagement/api/services/externalFlightsAPIService.ts
@@ -2,113 +2,122 @@ import axios from 'axios';
 import { ExternalAPIsConnectionConstants } from '../constants/externalAPIs/externalAPIsConnectionConstants';
 import { Input } from '../models/Input/input.model';
 import { ExternalFlightsAPIParametersModel } from '../models/Params/externalFlightsAPIParameters.model';
-import { setTimeout } from "timers/promises";
+import { setTimeout } from 'timers/promises';
 
 export class ExternalFlightsAPIService {
+	async getFlightsFromExternalAPI(params: ExternalFlightsAPIParametersModel) {
+		const options = {
+			method: 'GET',
+			url: ExternalAPIsConnectionConstants.flightsExternalAPI.url,
+			params: params,
+			headers: ExternalAPIsConnectionConstants.flightsExternalAPI.headers,
+		};
 
-    async getFlightsFromExternalAPI(params: ExternalFlightsAPIParametersModel) {
-        const options = {
-            method: 'GET',
-            url: ExternalAPIsConnectionConstants.flightsExternalAPI.url,
-            params: params,
-            headers: ExternalAPIsConnectionConstants.flightsExternalAPI.headers
-        };
-    
-        var result = await this.getExternalApiInformation(options);
-        console.log(result)
-        //result.itineraries.results
-        return result;
-    }
+		var result = await this.getExternalApiInformation(options);
+		console.log(result);
+		//result.itineraries.results
+		return result;
+	}
 
-    async getFlightsBetweenDates(req: Input) {
-        let requirements = structuredClone(req);
-        const start = requirements.startDate;
-        const end = requirements.endDate;
-        const duration = requirements.duration;
-        const durationMils = duration * 1440 * 60000;
+	async getFlightsBetweenDates(req: Input) {
+		let requirements = structuredClone(req);
+		const start = requirements.startDate;
+		const end = requirements.endDate;
+		const duration = requirements.duration;
+		const durationMils = duration * 1440 * 60000;
 
-        const infants = requirements.infants;
-        const children = requirements.children;
+		const infants = requirements.infants;
+		const children = requirements.children;
 
-        let departureParams: ExternalFlightsAPIParametersModel = {
-            adults: requirements.adults,
-            origin: requirements.origin.IATA,
-            destination: requirements.destination.IATA,
-            departureDate: requirements.startDate.toISOString(),
-            stops: '0,1,2',
-            currency: 'USD',
-        }
+		let departureParams: ExternalFlightsAPIParametersModel = {
+			adults: requirements.adults,
+			origin: requirements.origin.IATA,
+			destination: requirements.destination.IATA,
+			departureDate: requirements.startDate.toISOString(),
+			stops: '0,1,2',
+			currency: 'USD',
+			market: 'CO',
+			locale: 'es-MX',
+		};
 
-        let returnParams: ExternalFlightsAPIParametersModel = {
-            adults: requirements.adults,
-            destination: requirements.origin.IATA,
-            origin: requirements.destination.IATA,
-            departureDate: requirements.startDate.toISOString(),
-            stops: '0,1,2',
-            currency: 'USD',
-        }
+		let returnParams: ExternalFlightsAPIParametersModel = {
+			adults: requirements.adults,
+			destination: requirements.origin.IATA,
+			origin: requirements.destination.IATA,
+			departureDate: requirements.startDate.toISOString(),
+			stops: '0,1,2',
+			currency: 'USD',
+			market: 'CO',
+			locale: 'es-MX',
+		};
 
-        if (infants > 0 || children > 0) {
-            const kids = infants + children;  
-            if (kids <= 8) {
-              this.createChildsInParams(departureParams, 0, infants, 1);
-              this.createChildsInParams(departureParams, infants, kids, 10);
-              this.createChildsInParams(returnParams, 0, infants, 1);
-              this.createChildsInParams(returnParams, infants, kids, 10);
-            }
-        }
-    
-        let departureSegments: any[] = [];
-        let returnSegments: any[] = [];
-        while (end.getTime() - durationMils + 1440 * 60000 >= start.getTime()) {
-            const rt = new Date(start);
-            const h = new Date(rt.setDate(rt.getDate() + duration - 1));
-            var startString = start.toISOString().slice(0, 10);
-            var endString = h.toISOString().slice(0, 10);
+		if (infants > 0 || children > 0) {
+			const kids = infants + children;
+			if (kids <= 8) {
+				this.createChildsInParams(departureParams, 0, infants, 1);
+				this.createChildsInParams(departureParams, infants, kids, 10);
+				this.createChildsInParams(returnParams, 0, infants, 1);
+				this.createChildsInParams(returnParams, infants, kids, 10);
+			}
+		}
 
-            departureParams.departureDate = startString;
-            let statusDep = "incomplete"
-            let dep: any;
-            let d = 0;
+		let departureSegments: any[] = [];
+		let returnSegments: any[] = [];
+		while (end.getTime() - durationMils + 1440 * 60000 >= start.getTime()) {
+			const rt = new Date(start);
+			const h = new Date(rt.setDate(rt.getDate() + duration - 1));
+			var startString = start.toISOString().slice(0, 10);
+			var endString = h.toISOString().slice(0, 10);
 
-            //while (statusDep != "complete") {
-                console.log("dep", d++);
-                //await setTimeout(20000);
-                dep = await this.getFlightsFromExternalAPI(departureParams);
-                statusDep = dep.context.status;
-            //}
+			departureParams.departureDate = startString;
+			let statusDep = 'incomplete';
+			let dep: any;
+			let d = 0;
 
-            departureSegments = departureSegments.concat(dep.itineraries.results);
-            returnParams.departureDate = endString
-            let statusRet = "incomplete"
-            let ret: any;
-            let c = 0;
-            //while (statusRet != "complete") {
-                console.log("ret", c++ )
-            //    await setTimeout(20000);
-                ret = await this.getFlightsFromExternalAPI(returnParams);
-                statusRet = ret.context.status;
-            //}
+			//while (statusDep != "complete") {
+			console.log('dep', d++);
+			//await setTimeout(20000);
+			dep = await this.getFlightsFromExternalAPI(departureParams);
+			statusDep = dep.context.status;
+			//}
 
-            returnSegments = returnSegments.concat(ret.itineraries.results);
-            start.setDate(start.getDate() + 1);
-        }
+			departureSegments = departureSegments.concat(dep.itineraries.results);
+			returnParams.departureDate = endString;
+			let statusRet = 'incomplete';
+			let ret: any;
+			let c = 0;
+			//while (statusRet != "complete") {
+			console.log('ret', c++);
+			//    await setTimeout(20000);
+			ret = await this.getFlightsFromExternalAPI(returnParams);
+			statusRet = ret.context.status;
+			//}
 
-        return [departureSegments, returnSegments];
-    }
+			returnSegments = returnSegments.concat(ret.itineraries.results);
+			start.setDate(start.getDate() + 1);
+		}
 
-    createChildsInParams(params: ExternalFlightsAPIParametersModel, lower: number, upper:number, age: number) {
-        for (let i = lower; i < upper; i++) {
-          const str = "childAge" + (i+1) as keyof ExternalFlightsAPIParametersModel
-          params[str] = age;
-        }
-    }
+		return [departureSegments, returnSegments];
+	}
 
-    getExternalApiInformation(query: Object): Promise<any> {
-        const promise = axios.request(query);
-        const dataPromise = promise
-            .then((response: any) => response.data)
-            .catch((error: Error) => error);
-        return dataPromise;
-    }
+	createChildsInParams(
+		params: ExternalFlightsAPIParametersModel,
+		lower: number,
+		upper: number,
+		age: number
+	) {
+		for (let i = lower; i < upper; i++) {
+			const str = ('childAge' +
+				(i + 1)) as keyof ExternalFlightsAPIParametersModel;
+			params[str] = age;
+		}
+	}
+
+	getExternalApiInformation(query: Object): Promise<any> {
+		const promise = axios.request(query);
+		const dataPromise = promise
+			.then((response: any) => response.data)
+			.catch((error: Error) => error);
+		return dataPromise;
+	}
 }
